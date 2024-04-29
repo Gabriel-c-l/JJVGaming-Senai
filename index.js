deb4c4854a6a77a504babf9fea6af89011eeb829
Gabriel <Gabriel.c.lopes2004@gmail.com>
2024-04-29 19:36:10 -0300

    v1.0

--- index.js
+++ index.js
@@ -1,64 +1,84 @@
 let numeroMagico;
 let tentativas;
 
-function limparResposta() {
-    document.getElementById('resultado').innerText = '';
-}
-
 function selecionarModo(modo) {
-    limparResposta();
-    if (modo === 'pvp') {
+  if (modo === 'pvp') {
     document.getElementById('modoSelecao').style.display = 'none';
-    document.getElementById('inputNumero').style.display = 'block';
-    } else if (modo === 'pve') {
-    iniciarJogoPvE();
-    }
+    document.getElementById('inputNumeroPvP').style.display = 'block';
+  } else if (modo === 'pve') {
+    document.getElementById('modoSelecao').style.display = 'none';
+    document.getElementById('dificuldadeSelecao').style.display = 'block';
+  }
 }
 
-function iniciarJogo() {
-    limparResposta();
-    numeroMagico = parseInt(document.getElementById('numeroInput').value);
-    tentativas = 0;
-
-    document.getElementById('inputNumero').style.display = 'none';
-    document.getElementById('chuteDiv').style.display = 'block';
+function selecionarDificuldade(dificuldade) {
+  if (dificuldade === 'facil') {
+    iniciarJogoPvE(50); 
+  } else if (dificuldade === 'medio') {
+    iniciarJogoPvE(100); 
+  } else if (dificuldade === 'dificil') {
+    iniciarJogoPvE(200); 
+  }
 }
 
-function iniciarJogoPvE() {
-    limparResposta();
-    numeroMagico = Math.floor(Math.random() * 100) + 1;
-    tentativas = 0;
+function ocultarDificuldades() {
+  document.getElementById('dificuldadeSelecao').style.display = 'none';
+}
 
-    document.getElementById('modoSelecao').style.display = 'none';
-    document.getElementById('chuteDiv').style.display = 'block';
+function iniciarJogoPvP() {
+  numeroMagico = parseInt(document.getElementById('numeroInputJogador1').value);
+  tentativas = 0;
+
+  document.getElementById('inputNumeroPvP').style.display = 'none';
+  document.getElementById('chuteDiv').style.display = 'block';
+}
+
+function iniciarJogoPvE(maximo) {
+  numeroMagico = Math.floor(Math.random() * maximo) + 1;
+  tentativas = 0;
+
+  document.getElementById('inputNumeroPvE').style.display = 'none';
+  document.getElementById('chuteDiv').style.display = 'block';
 }
 
 function verificarChute() {
-    limparResposta();
-    const chute = parseInt(document.getElementById('chuteInput').value);
-    tentativas++;
+  const chute = parseInt(document.getElementById('chuteInput').value);
+  document.getElementById('chuteInput').focus();
+  document.getElementById('chuteInput').value = '';
+  tentativas++;
 
-    if (chute === numeroMagico) {
+  const diferenca = Math.abs(chute - numeroMagico);
+
+  if (chute === numeroMagico) {
     document.getElementById('resultado').innerText = `Parabéns! Você acertou o número mágico em ${tentativas} tentativas.`;
     document.getElementById('chuteDiv').style.display = 'none';
-    } else if (chute > numeroMagico) {
-    document.getElementById('resultado').innerText = "O número mágico é menor.";
-    } else {
-    document.getElementById('resultado').innerText = "O número mágico é maior.";
-    }
+  } else if (diferenca <= 2) {
+    document.getElementById('resultado').innerText = `Esta quase pegando fogo 🥵`;
+  } else if (diferenca <= 5) {
+    document.getElementById('resultado').innerText = `Esta esquentando 🔥🔥`;
+  } else if (diferenca <= 10) {
+    document.getElementById('resultado').innerText = `Morno`;
+  } else if (diferenca <= 15) {
+    document.getElementById('resultado').innerText = `Esta começando a esquentar 🧊`;
+  } else if (diferenca <= 20) {
+    document.getElementById('resultado').innerText = `pegue logo um cachecol esta muito frio 🧣❄️`;
+  } else if (diferenca <= 35) {
+    document.getElementById('resultado').innerText = `Mais um pouco e você chega no polo norte ⛄🐧🎅`;
+  } else {
+    document.getElementById('resultado').innerText = "Você está tão longe que nem sente mais as pernas de tanto frio 🥶";
+  }
 }
 
 function toggleModoNoturno() {
-    var modoNoturnoBtn = document.getElementById("modoNoturnoBtn");
-    var emojiAtual = modoNoturnoBtn.textContent;
-    var body = document.body;
-  
-    if (emojiAtual === "🌛") {
-      modoNoturnoBtn.textContent = "☀️"; 
-      body.classList.add("modo-noturno");
-    } else {
-      modoNoturnoBtn.textContent = "🌛"; 
-      body.classList.remove("modo-noturno"); 
-    }
+  var modoNoturnoBtn = document.getElementById("modoNoturnoBtn");
+  var emojiAtual = modoNoturnoBtn.textContent;
+  var body = document.body;
+
+  if (emojiAtual === "🌛") {
+    modoNoturnoBtn.textContent = "☀️"; 
+    body.classList.add("modo-noturno");
+  } else {
+    modoNoturnoBtn.textContent = "🌛"; 
+    body.classList.remove("modo-noturno"); 
   }
-  
+}
