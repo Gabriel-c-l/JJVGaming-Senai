8b64637f5ac21635d158e38b23d3ce9f5b90b422
Gabriel-C-Lopes <Gabriel.c.lopes2004@gmail.com>
2024-04-29 16:22:51 -0300

    v0.85

--- index.js
+++ index.js
@@ -1,41 +1,49 @@
 let numeroMagico;
-    let tentativas;
+let tentativas;
 
-    function selecionarModo(modo) {
-      if (modo === 'pvp') {
-        document.getElementById('modoSelecao').style.display = 'none';
-        document.getElementById('inputNumero').style.display = 'block';
-      } else if (modo === 'pve') {
-        iniciarJogoPvE();
-      }
+function limparResposta() {
+    document.getElementById('resultado').innerText = '';
+}
+
+function selecionarModo(modo) {
+    limparResposta();
+    if (modo === 'pvp') {
+    document.getElementById('modoSelecao').style.display = 'none';
+    document.getElementById('inputNumero').style.display = 'block';
+    } else if (modo === 'pve') {
+    iniciarJogoPvE();
     }
+}
 
-    function iniciarJogo() {
-      numeroMagico = parseInt(document.getElementById('numeroInput').value);
-      tentativas = 0;
+function iniciarJogo() {
+    limparResposta();
+    numeroMagico = parseInt(document.getElementById('numeroInput').value);
+    tentativas = 0;
 
-      document.getElementById('inputNumero').style.display = 'none';
-      document.getElementById('chuteDiv').style.display = 'block';
+    document.getElementById('inputNumero').style.display = 'none';
+    document.getElementById('chuteDiv').style.display = 'block';
+}
+
+function iniciarJogoPvE() {
+    limparResposta();
+    numeroMagico = Math.floor(Math.random() * 100) + 1;
+    tentativas = 0;
+
+    document.getElementById('modoSelecao').style.display = 'none';
+    document.getElementById('chuteDiv').style.display = 'block';
+}
+
+function verificarChute() {
+    limparResposta();
+    const chute = parseInt(document.getElementById('chuteInput').value);
+    tentativas++;
+
+    if (chute === numeroMagico) {
+    document.getElementById('resultado').innerText = `Parabéns! Você acertou o número mágico em ${tentativas} tentativas.`;
+    document.getElementById('chuteDiv').style.display = 'none';
+    } else if (chute > numeroMagico) {
+    document.getElementById('resultado').innerText = "O número mágico é menor.";
+    } else {
+    document.getElementById('resultado').innerText = "O número mágico é maior.";
     }
-
-    function iniciarJogoPvE() {
-      numeroMagico = Math.floor(Math.random() * 100) + 1;
-      tentativas = 0;
-
-      document.getElementById('modoSelecao').style.display = 'none';
-      document.getElementById('chuteDiv').style.display = 'block';
-    }
-
-    function verificarChute() {
-      const chute = parseInt(document.getElementById('chuteInput').value);
-      tentativas++;
-
-      if (chute === numeroMagico) {
-        document.getElementById('resultado').innerText = `Parabéns! Você acertou o número mágico em ${tentativas} tentativas.`;
-        document.getElementById('chuteDiv').style.display = 'none';
-      } else if (chute > numeroMagico) {
-        document.getElementById('resultado').innerText = "O número mágico é menor.";
-      } else {
-        document.getElementById('resultado').innerText = "O número mágico é maior.";
-      }
-    }
+}
